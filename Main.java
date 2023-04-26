f8cc19d9f871f0c333323e9f2991899801350832
andrewtlu <andrewlulu2012@gmail.com>
2023-04-26 11:26:26 -0400

    Add Main.java and Solution.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // TODO: switch statement calling respective method
+        Solution sol = new Solution();
+        sol.daylightTemp("./1089441.csv", null);
+    }
+}
---
+++ Solution.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Solution {
+    /** TODO: Implement
+     * Method 1
+     * <p>
+     * Calculates and prints average and sample standard deviation of dry-bulb temperatures (F) from sunrise to sunset of specified date
+     * 
+     * @param path path of csv data set
+     * @param date date to process
+     */
+    public void daylightTemp(String path, String date) {
+        
+    }
+
+    /** TODO: Implement
+     * Method 2
+     * <p>
+     * Prints all windchills of date when temperature is < 40 deg F
+     * 
+     * @param path path of csv data set
+     * @param date date to process
+     */
+    public void windchills(String path, String date) {
+
+    }
+
+    /** TODO: Implement
+     * Method 3
+     * <p>
+     * Finds and prints the most similar same day b/t two data sets
+     * 
+     * @param path1 path of first csv data set
+     * @param path2 path of second csv data set
+     */
+    public void similarDay(String path1, String path2) {
+
+    }
+}
