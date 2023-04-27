b7f41cf658d85be5fb468d061c03bf831fb6caf3
andrewtlu <andrewlulu2012@gmail.com>
2023-04-26 23:57:55 -0400

    Finish main

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        // testing commands:
+        // javac *.java; java Main  daylight_temp ./1089419.csv 2017-01-01
+        // javac *.java; java Main windchills ./1089419.csv 2017-01-06
+        // javac *.java; java Main similar-day ./1089419.csv ./1089441.csv
+
         Solution sol = new Solution();
 
         switch (args[0]) {
@@ -7,9 +12,5 @@ public class Main {
             case "windchills" -> sol.windchills(args[1], args[2]);
             case "similar-day" -> sol.similarDay(args[1], args[2]);
         }
-
-        // sol.daylightTemp("./1089419.csv", "2017-01-01");
-        // sol.windchills("./1089419.csv", "2017-01-06");
-        // sol.similarDay("./1089419.csv", "./1089441.csv");
     }
 }
