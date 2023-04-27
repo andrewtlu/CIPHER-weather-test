5eab76ff92e89f5e8a991084016b0b8f4c01de01
andrewtlu <andrewlulu2012@gmail.com>
2023-04-26 23:49:46 -0400

    Clean up solutions and create Data static class

--- Solution.java
+++ Solution.java
@@ -14,28 +14,29 @@ public class Solution {
      * @param date date to process
      */
     public void daylightTemp(String path, String date) {
-        String line;
-        String[] data;
+        String row;
+        Data data;
+
         double avg = 0;
         int sunrise = -1, sunset = -1, time, temp;
         List<Double> temps = new ArrayList<>();
         date = convert(date);
     
         try (BufferedReader reader = new BufferedReader(new FileReader(path));) {
-            while ((line = reader.readLine()) != null) {
-                if (!line.contains(date)) continue;
+            while ((row = reader.readLine()) != null) {
+                if (!row.contains(date)) continue;
                 
-                data = line.split(",");
+                data = new Data(row);
                 
-                if (sunrise == -1) { sunrise = Integer.parseInt(data[35]); sunset = Integer.parseInt(data[36]); }
-                time = Integer.parseInt(data[5].split(" ")[1].replaceAll(":", ""));
+                if (sunrise == -1) { sunrise = data.getSunrise(); sunset = data.getSunset(); }
+                time = data.getTime();
                 
                 if (time < sunrise) continue; // INSTRUCTIONS UNCLEAR, BETWEEN IS INCLUSIVE IN MY IMPLEMENTATION
                 else if (time > sunset) break;
                 else {
-                    if (data[10].isEmpty()) continue;
+                    if (!data.hasTemp()) continue;
 
-                    temp = temp(data[10]);
+                    temp = data.getTemp();
                     avg += temp;
                     temps.add((double)temp);
                 }
@@ -61,26 +62,27 @@ public class Solution {
      * @param date date to process
      */
     public void windchills(String path, String date) {
-        String line;
-        String[] data;
+        String row;
+        Data data;
+
         boolean reached = false;
         int temp;
         date = convert(date);
     
         try (BufferedReader reader = new BufferedReader(new FileReader(path));) {
-            while ((line = reader.readLine()) != null) {
-                data = line.split(",");
-
-                if (!data[5].contains(date)) {
+            while ((row = reader.readLine()) != null) {
+                if (!row.contains(date)) {
                     if (!reached) continue;
                     else break;
                 }
+
                 reached = true;
+                data = new Data(row);
 
-                if (data[10].isEmpty() || data[17].isEmpty()) continue;
+                if (!data.hasTemp() || !data.hasSpeed()) continue;
 
-                temp = temp(data[10]);
-                if (temp <= 40) System.out.println(windchill(temp, Integer.parseInt(data[17])));
+                temp = data.getTemp();
+                if (temp <= 40) System.out.println(windchill(temp, data.getSpeed()));
             }
         } catch (Exception ex) { ex.printStackTrace(); }
     }
@@ -89,21 +91,21 @@ public class Solution {
      * Method 3
      * <p>
      * Finds and prints the most similar same day b/t two data sets
+     * <p>
+     * Utilizes unweighted euclidean distance of temperature, humidity, and wind speed
      * 
      * @param path1 path of first csv data set
      * @param path2 path of second csv data set
      */
     public void similarDay(String path1, String path2) {
+        String row1 = null, row2 = null, match = null;
         Day day;
-        String line1 = null, line2 = null, match = null;
-        String[] data;
         int diff, similar = -1;
 
         // factor out
         Day[] days = new Day[2];
-        String[] lines = {line1, line2}, dates = new String[2];
-        String[][] datas = new String[2][];
-
+        String[] rows = {row1, row2}, dates = new String[2];
+        Data[] datas = new Data[2];
 
         try (
             BufferedReader reader1 = new BufferedReader(new FileReader(path1));
@@ -112,37 +114,33 @@ public class Solution {
             reader1.readLine(); reader2.readLine(); // remove header
             BufferedReader[] readers = {reader1, reader2};
 
-            while ((line1 = reader1.readLine()) != null && (line2 = reader2.readLine()) != null) {
-                // find the same date
+            while ((row1 = reader1.readLine()) != null && (row2 = reader2.readLine()) != null) {
+                // 1 find the same date
                 do {
-                    lines[0] = line1; lines[1] = line2;
+                    rows[0] = row1; rows[1] = row2;
 
                     for (int i = 0; i < 2; i++) {
-                        datas[i] = lines[i].split(",");
-                        dates[i] = datas[i][5].split(" ")[0];
+                        datas[i] = new Data(rows[i]);
+                        dates[i] = datas[i].getDate();
                     }
                     diff = compareDates(dates[0], dates[1]);
 
-                    if (diff > 0) line2 = reader2.readLine();
-                    else if (diff < 0) line1 = reader1.readLine();
+                    if (diff > 0) row2 = reader2.readLine();
+                    else if (diff < 0) row1 = reader1.readLine();
                 } while (diff != 0);
 
-                // load each day's entirety
+                // 2 load each day's entirety
                 for (int i = 0; i < 2; i++) {
                     days[i] = new Day(dates[i]);
                     day = days[i];
                     
                     do {
-                        datas[i] = lines[i].split(",");
-                        data = datas[i];
-
-                        day.addTemp(data[10]);
-                        day.addHumidity(data[16]);
-                        day.addSpeed(data[17]);
-                    } while ((lines[i] = readers[i].readLine()) != null && lines[i].contains(dates[i]));
+                        datas[i] = new Data(rows[i]);
+                        day.addAll(datas[i]);
+                    } while ((rows[i] = readers[i].readLine()) != null && rows[i].contains(dates[i]));
                 }
                 
-                // check if more similar
+                // 3 check if more similar
                 diff = days[0].compareTo(days[1]);
                 if (similar == -1 || similar > diff && diff != -1) {
                     similar = diff;
@@ -174,16 +172,6 @@ public class Solution {
         // convert to MM/dd/yyyy
         return String.format("%d/%d/%02d", split[1], split[2], split[0] % 100);
     }
-
-    /**
-     * Trims and returns integer temperature
-     * 
-     * @param temp
-     * @return
-     */
-    public int temp(String temp) {
-        return Integer.parseInt(temp.replaceAll("s", "")); // remove suspect marker
-    }
  
     /**
      * Calculates windchill factor given temp and wind speed
@@ -209,6 +197,54 @@ public class Solution {
         return 0;
     }
 
+    public static class Data {
+        String[] data;
+
+        public Data(String row) {
+            this.data = row.split(",");
+        }
+
+        public int getTime() {
+            return Integer.parseInt(data[5].split(" ")[1].replaceAll(":", ""));
+        }
+
+        public String getDate() {
+            return data[5].split(" ")[0];
+        }
+
+        public boolean hasTemp() {
+            return !data[10].isEmpty();
+        }
+
+        public int getTemp() {
+            return Integer.parseInt(data[10].replaceAll("s", "")); 
+        }
+
+        public boolean hasHumidity() {
+            return !data[16].isEmpty();
+        }
+
+        public int getHumidity() {
+            return Integer.parseInt(data[16]);
+        }
+
+        public boolean hasSpeed() {
+            return !data[17].isEmpty();
+        }
+
+        public int getSpeed() {
+            return Integer.parseInt(data[17].replaceAll("s", ""));
+        }
+
+        public int getSunrise() {
+            return Integer.parseInt(data[35]);
+        }
+
+        public int getSunset() {
+            return Integer.parseInt(data[36]);
+        }
+    }
+
     public static class Day implements Comparable<Day> {
         int tempCount, humidityCount, speedCount;
         double temp, humidity, speed;
@@ -225,21 +261,27 @@ public class Solution {
             tempCount = humidityCount = speedCount = 0;
         }
 
-        public void addTemp(String temp) {
-            if (temp.isEmpty()) return;
-            this.temp += Integer.parseInt(temp.replaceAll("s", ""));
+        public void addAll(Data data) {
+            addTemp(data);
+            addHumidity(data);
+            addSpeed(data);
+        }
+
+        public void addTemp(Data data) {
+            if (!data.hasTemp()) return;
+            this.temp += data.getTemp();
             tempCount++;
         }
 
-        public void addHumidity(String humidity) {
-            if (humidity.isEmpty()) return;
-            this.humidity += Integer.parseInt(humidity);
+        public void addHumidity(Data data) {
+            if (!data.hasHumidity()) return;
+            this.humidity += data.getHumidity();
             humidityCount++;
         }
 
-        public void addSpeed(String speed) {
-            if (speed.isEmpty()) return;
-            this.speed += Integer.parseInt(speed.replaceAll("s", ""));
+        public void addSpeed(Data data) {
+            if (!data.hasSpeed()) return;
+            this.speed += data.getSpeed();
             speedCount++;
         }
 
