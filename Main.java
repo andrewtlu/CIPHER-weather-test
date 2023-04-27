500a5578b5145da513a6f00c1b6966398eca5969
andrewtlu <andrewlulu2012@gmail.com>
2023-04-26 23:53:08 -0400

    Add switch logic control for main

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO: switch statement calling respective method
         Solution sol = new Solution();
+
+        switch (args[0]) {
+            case "daylight_temp" -> sol.daylightTemp(args[1], args[2]);
+            case "windchills" -> sol.windchills(args[1], args[2]);
+            case "similar-day" -> sol.similarDay(args[1], args[2]);
+        }
+
         // sol.daylightTemp("./1089419.csv", "2017-01-01");
-        sol.windchills("./1089419.csv", "2017-01-06");
+        // sol.windchills("./1089419.csv", "2017-01-06");
+        // sol.similarDay("./1089419.csv", "./1089441.csv");
     }
 }
